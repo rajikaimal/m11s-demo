f3b9c264fd87bbb80391505b7a216c3ab68fe5aa
rajikaimal <inbox.rajika@gmail.com>
2020-04-25 11:18:42 +0530

    Add README.md

--- src/post-service/db.js
+++ src/post-service/db.js
@@ -3,7 +3,7 @@ const username = process.env.dbUsername;
 const password = process.env.dbPassword;
 
 async function connectMongo() {
-	await mongoose.connect(`mongodb://${username}:${password}@ds037498.mlab.com:37498/fiction-paper`, {
+	await mongoose.connect(`${process.env.dbConnection}`, {
   	useNewUrlParser: true,
   	useUnifiedTopology: true
 	});
